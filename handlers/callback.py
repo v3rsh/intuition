55d8e8f6f394d7dfebdf40a2b5fa874df260ff80
Mike <mikhail.vershkov@gmail.com>
2025-02-28 11:15:51 +0300

    `Removed inline message sending and replaced with call to send_welcome_message function`

--- handlers/callback.py
+++ handlers/callback.py
@@ -1,7 +1,7 @@
 from aiogram import Router
 from aiogram.types import CallbackQuery
 from aiogram.fsm.context import FSMContext
-
+from utils import send_welcome_message
 from states import BotState
 from keyboards import main_menu_kb
 
@@ -10,9 +10,6 @@ router = Router()
 @router.callback_query(lambda call: call.data == "to_main")
 async def callback_to_main(query: CallbackQuery, state: FSMContext):
     await state.set_state(BotState.MAIN_MENU)
-    await query.message.answer(
-        "Вы перешли в главное меню",
-        reply_markup=main_menu_kb()
-    )
+    await send_welcome_message(query.message)
     # Также не забываем answerCallbackQuery
     await query.answer()
